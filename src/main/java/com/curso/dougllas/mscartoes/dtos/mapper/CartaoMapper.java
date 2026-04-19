3d989709ff39a370ad1609111db7fdf88e6a75b0
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-18 22:24:25 -0300

    feat: Adiciona Component no Mapper para ser reconhecido no build

--- src/main/java/com/curso/dougllas/mscartoes/dtos/mapper/CartaoMapper.java
+++ src/main/java/com/curso/dougllas/mscartoes/dtos/mapper/CartaoMapper.java
@@ -3,7 +3,9 @@ package com.curso.dougllas.mscartoes.dtos.mapper;
 import com.curso.dougllas.mscartoes.dtos.CartaoRequestDTO;
 import com.curso.dougllas.mscartoes.entities.Cartao;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface CartaoMapper {
 
