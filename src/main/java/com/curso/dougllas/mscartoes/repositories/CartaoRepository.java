f763083fad81074f3b718ad7fe55fd3eb1ff8aaa
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-18 22:07:05 -0300

    feat: Adiciona consulta por renda

--- src/main/java/com/curso/dougllas/mscartoes/repositories/CartaoRepository.java
+++ src/main/java/com/curso/dougllas/mscartoes/repositories/CartaoRepository.java
@@ -4,6 +4,10 @@ import com.curso.dougllas.mscartoes.entities.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+
 @Repository
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
+
+    Cartao findByRenda(BigDecimal renda);
 }
