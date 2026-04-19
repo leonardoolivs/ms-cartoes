f8f40ba52fd35adc2790606c0d38ffeb1efad4fe
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-18 22:02:38 -0300

    feat: Adiciona Mapper de Cartao

---
+++ src/main/java/com/curso/dougllas/mscartoes/dtos/mapper/CartaoMapper.java
@@ -0,0 +1,13 @@
+package com.curso.dougllas.mscartoes.dtos.mapper;
+
+import com.curso.dougllas.mscartoes.dtos.CartaoRequestDTO;
+import com.curso.dougllas.mscartoes.entities.Cartao;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CartaoMapper {
+
+    Cartao toEntity(CartaoRequestDTO dto);
+    
+    CartaoRequestDTO toDto(Cartao entity);
+}
