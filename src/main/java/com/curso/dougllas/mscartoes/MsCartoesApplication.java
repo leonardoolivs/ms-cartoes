98110a913c82ea586900f1bf68e5bb07fce339e3
Leonardo Oliveira <leonardo.sousaoliver@gmail.com>
2026-04-18 21:08:56 -0300

    build: Cria o projeto com Lombok,H2, Spring Web e JPA

---
+++ src/main/java/com/curso/dougllas/mscartoes/MsCartoesApplication.java
@@ -0,0 +1,13 @@
+package com.curso.dougllas.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCartoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCartoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curso/dougllas/mscartoes/MsCartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.dougllas.mscartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCartoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
